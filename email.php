ced56c577bfa422e0fdc9e7fbe56203d5d7cd379
AkkiLingwal96 <akkilingwal@gmail.com>
2017-07-17 21:24:26 +0530

    Add files via upload

--- email.php
+++ email.php
@@ -1,9 +1,54 @@
+<html>
+    <head>
+        <title>Success</title>
+        <link rel="apple-touch-icon-precomposed" sizes="57x57" href="icon/apple-touch-icon-57x57.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="icon/apple-touch-icon-114x114.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="icon/apple-touch-icon-72x72.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="icon/apple-touch-icon-144x144.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="60x60" href="icon/apple-touch-icon-60x60.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="120x120" href="icon/apple-touch-icon-120x120.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="76x76" href="icon/apple-touch-icon-76x76.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="152x152" href="icon/apple-touch-icon-152x152.png" />
+        <link rel="icon" type="image/png" href="icon/favicon-196x196.png" sizes="196x196" />
+        <link rel="icon" type="image/png" href="icon/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/png" href="icon/favicon-32x32.png" sizes="32x32" />
+        <link rel="icon" type="image/png" href="icon/favicon-16x16.png" sizes="16x16" />
+        <link rel="icon" type="image/png" href="icon/favicon-128.png" sizes="128x128" />
+        <meta name="application-name" content="&nbsp;"/>
+        <meta name="msapplication-TileColor" content="#FFFFFF" />
+        <meta name="msapplication-TileImage" content="mstile-144x144.png" />
+        <meta name="msapplication-square70x70logo" content="mstile-70x70.png" />
+        <meta name="msapplication-square150x150logo" content="mstile-150x150.png" />
+        <meta name="msapplication-wide310x150logo" content="mstile-310x150.png" />
+        <meta name="msapplication-square310x310logo" content="mstile-310x310.png" />
+
+        
+        <link rel='stylesheet' type='text/css' href='stylesheet.css'>
+        <link rel='stylesheet' type='text/css' href='mastercss.css'>
+        <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="HandheldFriendly" content="true">
+    </head>
+    <body bgcolor="black">
+        <center><div style="width: 90%;" align="center">
+            <center><img src="banner2.png" alt="downloadmp3andalbums" style="background-size: cover; width: 90%; height: 90%;"></center>            
+                <div style="width: 90%;" class="topnav" id="myTopnav">
+                        <a href="master.html">Home</a>
+                        <a href="news.html">News</a>
+                        <a href="download.html">Download</a>
+                        <a href="batches.html">Batches</a>
+                        <a href="request.html">Request</a>
+                        <a href="about.html">About</a>
+                        <a href="contact.html">Contact</a>                         
+                        <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>                    
+                    </div>
+            </div>                
 <?php
 
 function post_captcha($user_response) {
         $fields_string = '';
         $fields = array(
-            'secret' => '6LcYvCgUAAAAAEIr2bpnBQvAWGy9Zzbo_Wh_igBl',
+            'secret' => '6LdQBykUAAAAAAFLT1DLgp_ccXGVpEor-Z-S-Njh',
             'response' => $user_response
         );
         foreach($fields as $key=>$value)
@@ -27,14 +72,17 @@ function post_captcha($user_response) {
 
     if (!$res['success']) {
         // What happens when the CAPTCHA wasn't checked
-        echo '<h2 style="color: white; font-family: calibri;">Please go back and make sure you check the security CAPTCHA box.</h2><br>';
-		
+?>
+     
+        <h2 style="color: white; font-family: calibri;">Please go back and make sure you check the security CAPTCHA box.</h2><br>
+		                   
+<?php
 		
     } else {
 		
 if(isset($_POST['email'])) {
  
-    $email_to = "mygaragesounds@gmail.com";
+    $email_to = "blackcattunes@gmail.com";
      $email_subject = "Contact";
  
     $name = $_POST['name']; // required
@@ -63,12 +111,25 @@ $headers = 'From: '.$email_from."\r\n".
 'X-Mailer: PHP/' . phpversion();
 @mail($email_to, $email_subject, $email_message, $headers);  
 ?>
- 
-
- 
-<h2 style="color: white; font-family: calibri;">Thank you for contacting us. We will be in touch with you very soon. </h2>
- 
+<h2 style="color: white; font-family: calibri; font-size: large;">Thank you for contacting us. We will be in touch with you very soon.</h2>
 <?php
-}
-}
+    }
+    }
 ?>
+	
+	<div align="center" style="color: white; width: 90%; font-size: small;">
+                <footer>&copy; 2017 BlackCatTunes. All rights reserved.</footer>
+                </div>
+        </center>    
+    <script>
+            function myFunction() {
+            var x = document.getElementById("myTopnav");
+            if (x.className === "topnav") {
+                x.className += " responsive";
+            } else {
+                x.className = "topnav";
+            }
+        }
+</script>
+    </body>
+</html>
